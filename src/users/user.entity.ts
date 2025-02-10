c291a4235f23925b09251e27b77df626e814af1c
Prabin Nepal <prabinnepal1996@gmail.com>
2025-02-10 17:35:11 -0500

    One-to-one relationship

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Exclude, Expose } from 'class-transformer';
+import Address from './address.entity';
 
 @Entity()
 class User {
@@ -17,6 +24,10 @@ class User {
   @Column()
   @Exclude()
   public password: string;
+
+  @OneToOne(() => Address)
+  @JoinColumn()
+  public address: Address;
 }
 
 export default User;
